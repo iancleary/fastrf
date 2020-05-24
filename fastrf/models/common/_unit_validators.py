8855d7bd6fe4830cbd2cd2f3f5cd6f6c3ddf1679
iancleary <iancleary@pm.me>
2020-05-24 16:54:34 -0700

    Disabled until used

--- fastrf/models/common/_unit_validators.py
+++ fastrf/models/common/_unit_validators.py
@@ -13,34 +13,34 @@ def dB_unit_check_validator():
 # ----------------------------------------------------------------------
 
 
-def dBc_unit_check(unit: str) -> None:
-    if unit != "dBc":
-        raise ValueError("unit: {unit}, must be dBc".format(unit=unit))
+# def dBc_unit_check(unit: str) -> None:
+#     if unit != "dBc":
+#         raise ValueError("unit: {unit}, must be dBc".format(unit=unit))
 
 
-def dBc_unit_check_validator():
-    return validator("unit", allow_reuse=True)(dBc_unit_check)
+# def dBc_unit_check_validator():
+#     return validator("unit", allow_reuse=True)(dBc_unit_check)
 
 
-# ----------------------------------------------------------------------
+# # ----------------------------------------------------------------------
 
 
-def dBm_unit_check(unit: str) -> None:
-    if unit != "dBm":
-        raise ValueError("unit: {unit}, must be dBm".format(unit=unit))
+# def dBm_unit_check(unit: str) -> None:
+#     if unit != "dBm":
+#         raise ValueError("unit: {unit}, must be dBm".format(unit=unit))
 
 
-def dBm_unit_check_validator():
-    return validator("unit", allow_reuse=True)(dBm_unit_check)
+# def dBm_unit_check_validator():
+#     return validator("unit", allow_reuse=True)(dBm_unit_check)
 
 
-# ----------------------------------------------------------------------
+# # ----------------------------------------------------------------------
 
 
-def dBW_unit_check(unit: str) -> None:
-    if unit != "dBW":
-        raise ValueError("unit: {unit}, must be dBW".format(unit=unit))
+# def dBW_unit_check(unit: str) -> None:
+#     if unit != "dBW":
+#         raise ValueError("unit: {unit}, must be dBW".format(unit=unit))
 
 
-def dBW_unit_check_validator():
-    return validator("unit", allow_reuse=True)(dBW_unit_check)
+# def dBW_unit_check_validator():
+#     return validator("unit", allow_reuse=True)(dBW_unit_check)
