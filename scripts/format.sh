5f1580a1afd7e13fbe4d7737142841a63f51b842
iancleary <iancleary@hey.com>
2020-07-10 17:45:38 -0700

    Updated isort commands

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,4 +3,4 @@ set -x
 
 autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place fastrf tests --exclude=__init__.py
 black fastrf tests
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --thirdparty fastrf --apply fastrf tests
+isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --project fastrf fastrf tests
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,4 +5,4 @@ set -x
 
 mypy fastrf --disallow-untyped-defs
 black fastrf tests --check
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty fastrf fastrf tests
+isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --check-only --project fastrf --project tests fastrf tests
