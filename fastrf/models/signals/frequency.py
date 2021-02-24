cd5cb66e6606896adb780b6a57ab3fa0e5ce4a30
iancleary <iancleary@hey.com>
2021-02-23 20:08:57 -0700

    mypy ignore

--- fastrf/models/signals/frequency.py
+++ fastrf/models/signals/frequency.py
@@ -1,11 +1,12 @@
 from fastapi_camelcase import CamelModel
 from pydantic import confloat
 
-from ..common._unit_validators import frequency_unit_validator
+from fastrf.models.common._unit_validators import frequency_unit_validator
 
 
 class Frequency(CamelModel):
-    value: confloat(strict=True, ge=0.0)  # Assuming negative frequencies are not needed
+    value: confloat(strict=True, ge=0.0) # type: ignore
+    # Assuming negative frequencies are not needed
     unit: str = "Hz"  # SI Unit
 
     _unit_must_be_in_allowed_set = frequency_unit_validator()
